2d789729f61cb1d43b0fdf7fd0a43647eb6a6730
emilytsao168 <117272534+emilytsao168@users.noreply.github.com>
2024-05-04 01:09:34 +0800

    emily

--- Emily_README.md
+++ Emily_README.md
@@ -12,17 +12,18 @@
 ### 功能:  
 1.  #### 把相同flow的資料存成dict   
 2.  #### 每個dict包含(pkt_size, time_diff, original_class)   
-3.  #### 刪除只有標題的檔案   
+3.  #### 刪除只有標題的檔案，以及只有一個package的flow   
 4.  #### 紀錄size跟package的最大值及最小值   
-5.  #### 儲存處裡好的資料至csv檔案中，檔名與原輸入檔案相同   
+5.  #### 儲存處裡好的資料至csv檔案中，路徑為(output_file_path+原檔案名稱+pkt_size_time_diff)   
 6.  #### 每個csv包含(src_ip, dest_ip, pkt_size, time_diff, original_class)   
 #### 注意！在讀取 CSV 檔案後，資料將以列表（list）的形式被讀取   
 
-![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/235346.png)   
-![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/235702.png)   
-![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/235622.png)   
+![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/005710.png)   
+![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/005541.png)   
+![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/005630.png)   
+![圖片](/D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/PICTURES/005759.png)   
    
-##### 2024/05/03 更新   
+##### 2024/05/04 更新   
    
 # calculate_anomaly_score.py  
 -------------  
--- to_flow.py
+++ to_flow.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 def process_traffic_data(input_file, output_file, original_class):
-    
+
     file_list = os.listdir(input_file)
     max_time_diff = float('-inf')
     min_time_diff = float('inf')
@@ -24,7 +24,7 @@ def process_traffic_data(input_file, output_file, original_class):
 
                 if num_rows > 1:
                     for src_ip, dest_ip, pkt_size, time_diff in reader:
-                        
+
                         pkt_size = int(pkt_size)
                         time_diff = float(time_diff)
                         max_pkt_size = max(max_pkt_size, pkt_size)
@@ -36,26 +36,33 @@ def process_traffic_data(input_file, output_file, original_class):
                         if flow_key not in flows:
                             flows[flow_key] = []
                         flows[flow_key].append({'pkt_size': int(pkt_size), 'time_diff': float(time_diff), 'original_class': original_class})
-            
-                    # write_csv
-                    output_file_path = os.path.join(output_file, filename)
-                    with open(output_file_path, 'w', newline='') as f:
-                        fieldnames = ['src_ip', 'dest_ip', 'pkt_size', 'time_diff', 'original_class']
-                        writer = csv.DictWriter(f, fieldnames=fieldnames)
-                        writer.writeheader()
-                        for flow_key, flow_data in flows.items():
-                            for packet in flow_data:
-                                writer.writerow({'src_ip': flow_key[0], 'dest_ip': flow_key[1], 'pkt_size': packet['pkt_size'], 'time_diff': packet['time_diff'], 'original_class': packet['original_class']})
+                    
+                    for flow_key, flow_data in flows.items():
+                        
+                        # the number of package > 1
+                        if len(flow_data) > 1:
+                            flow_dir = os.path.join(output_file, filename)
+                            os.makedirs(flow_dir, exist_ok=True)
+                            output_file_path = os.path.join(flow_dir, f"{flow_key[0]}_{flow_key[1]}.csv")
+
+                            # write_csv
+                            with open(output_file_path, 'w', newline='') as f:
+                                fieldnames = ['src_ip', 'dest_ip', 'pkt_size', 'time_diff', 'original_class']
+                                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                                writer.writeheader()
+
+                                for packet in flow_data:
+                                    writer.writerow({'src_ip': flow_key[0], 'dest_ip': flow_key[1], 'pkt_size': packet['pkt_size'], 'time_diff': packet['time_diff'], 'original_class': packet['original_class']})
                 else:
                     print(f"File {filename} skipped because it only contains header.")
-    
+
     print("\nFile output completed!!!\n")
     print(f"Max pkt_size: {max_pkt_size}")
     print(f"Min pkt_size: {min_pkt_size}")
     print(f"Max time_diff: {max_time_diff}")
     print(f"Min time_diff: {min_time_diff}")
 
-# TEST (input_file_path, output_file_path,  original_class)
-process_traffic_data('D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/Final_Project/NonVPNcsv-01/',
-                      'D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/TEST_NonVPNcsv-01/', 
-                      'regular http traffic')
+# TEST
+process_traffic_data('D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/Final_Project/NonVPNcsv-01/', 
+                     'D:/EMILY/emily/ncu/112-2/CO3005/Computer_Networks_Final_Project/TEST_NonVPNcsv-01/', 
+                     'regular http traffic')
