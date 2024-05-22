3fb30519cafdeb272002c150281a1021a354c1d3
jimmyhuang <127335630+helloiamhandsome@users.noreply.github.com>
2024-05-22 21:21:11 +0800

    Update paint.py

--- paint.py
+++ paint.py
@@ -4,23 +4,20 @@ import csv
 import os
 import pandas as pd
 
-#regular http traffic
-#tunneling http traffic
-
 plt.figure(figsize=(6,5))
 
-x = np.array([0,1,2,3,4,5,6,7,8,9,10,11])
+x = np.array([2,3,4,5,6,7,8,9,10,11])
 y = np.array([0.4,0.5,0.6,0.7,0.8,0.9,1.0])
 
-xgroup_labels=['0','1','2','3','4','5','6','7','8','9','10','11']   #x軸刻度
+xgroup_labels=['2','3','4','5','6','7','8','9','10','11']   #x軸刻度
 ygroup_labels=['0.4','0.5','0.6','0.7','0.8','0.9','1.0']   #y軸刻度
 
-plt.xticks(x,xgroup_labels,fontsize=18,fontweight='bold')
-plt.yticks(y,ygroup_labels,fontsize=18,fontweight='bold')
+plt.xticks(x,xgroup_labels,fontsize=12,fontweight='bold')
+plt.yticks(y,ygroup_labels,fontsize=12,fontweight='bold')
 
-plt.xlabel("N_sects",fontsize=20,fontweight='bold')
-plt.ylabel("Hit ratio",fontsize=20,fontweight='bold')
-plt.xlim(0,11)   #x軸範圍
+plt.xlabel("N_sects",fontsize=14,fontweight='bold')
+plt.ylabel("Hit ratio",fontsize=14,fontweight='bold')
+plt.xlim(1.5,10.5)   #x軸範圍
 plt.ylim(0.4,1.0)   #y軸範圍
 
 def compare_classes(original_class, new_class):
@@ -48,27 +45,32 @@ def process_csv_file(csv_file):
 def process_folder(folder_path):
     
     total_rows = 0
-    N = 1
+    N = 2
+    N_list = []
+    hit_ratio_list = []
+    
     # 遍歷資料夾內的檔案
     for filename in os.listdir(folder_path):
         if filename.endswith('.csv'):
             csv_file_path = os.path.join(folder_path, filename)
             a, rows = process_csv_file(csv_file_path)
             hit_ratio = a/rows
+            N_list.append(N)
+            hit_ratio_list.append(hit_ratio)
             N += 1
-            print("N:", N)
-            print("hit_ratio:", hit_ratio)
             
-            plt.plot(N, hit_ratio, color = 'blue', marker = 'x', label='HTTP') 
+            print("N:", N-1)
+            print("hit_ratio:", hit_ratio)
 
-            if N ==10:
+            if N == 11:
                 break
 
-    return total_rows, N, hit_ratio
+    return N_list, hit_ratio_list
 
-folder_path = 'D:/Anomaly_Score'  
-N, total_rows, hit_ratio = process_folder(folder_path)
-print("Total rows processed across all CSV files:", total_rows)
-plt.legend(loc='upper right')  #添加圖例並設定位置在右上角
+folder_path = 'D:/Anomaly_Score'  # 請替換為你的資料夾路徑
+N_list, hit_ratio_list = process_folder(folder_path)
+
+plt.plot(N_list, hit_ratio_list, color='blue', marker='x', linestyle='-', label='HTTP') 
+plt.legend(loc='upper right')  # 添加圖例並設置位置為右上角
 
 plt.show()
